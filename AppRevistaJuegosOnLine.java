ae136be5880384b24edc63a1094873ee8da8e98f
Oskar Contreras <ocontrerasjdam1@gmail.com>
2020-01-22 16:50:13 +0100

    Main Finalizado

--- AppRevistaJuegosOnLine.java
+++ AppRevistaJuegosOnLine.java
@@ -1,3 +1,7 @@
+
+
+import java.util.Arrays;
+
 /**
  * Punto de entrada a la aplicación
  * 
@@ -5,11 +9,39 @@
  */
 public class AppRevistaJuegosOnLine 
 {
-	public static void main(String[] args)
-	{
-		
+    public static void main(String[] args)
+    {
+        if(args.length != 2){
+            System.out.println("Error en argumentos" + 
+                "\nSintaxis: java AppRevistaJuegosOnLine <nombre> <n>");
+        }else{
+            String nombre = args[0];
+            int total = Integer.parseInt(args[1]);
+            RevistaOnLineJuegos revista = new RevistaOnLineJuegos(nombre,total);
 
-		 
-	}
+            revista.leerDeFichero();
+            System.out.println(revista.toString());
+            System.out.println("***************************************");
+            System.out.println("Puntuando...");
+            revista.puntuar("Planet Zoo", 8);
+            revista.puntuar("Steep", 7);
+            revista.puntuar("Catastronauts", 9);
+            revista.puntuar("Wattam", 9);
+            System.out.println("Tras puntuar la revista queda\n");
+            System.out.println(revista.toString());
+            System.out.println("***************************************");
+            System.out.println("Los juegos con valoracion superior a 8.2 son: ");
 
+            String[] valoracionSuperiorA = revista.valoracionMayorQue(8.2);
+            System.out.println(Arrays.toString(valoracionSuperiorA));
+
+            System.out.println("***************************************");
+            System.out.println("Borrando juegos de genero ROL... ");
+            System.out.println("Borrados " + revista.borrarDeGenero(Genero.ROL) + "juegos.");
+
+            System.out.println(revista.toString());
+
+            
+        }
+    }
 }
---
+++ Genero.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Genero here.
+ * 
+ * @author Oskar Contreras 
+ * @version (a version number or a date)
+ */
+public enum Genero
+{
+   AVENTURA, DEPORTE, ESTRATEGIA, ROL
+
+}
